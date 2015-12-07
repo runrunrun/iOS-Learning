58b73330e57d1daacd1a1fef14481a363bd2afc5
Hari Kunwar <hkunwar>
2015-12-07 11:33:00 -0800

    Framework sample updates.

--- SampleFramework/FrameworkSample/FrameworkSample/ViewController.swift
+++ SampleFramework/FrameworkSample/FrameworkSample/ViewController.swift
@@ -1,25 +1,23 @@
 //
 //  ViewController.swift
-//  SampleTest
+//  FrameworkSample
 //
 //  Created by Hari Kunwar on 12/7/15.
-//  Copyright (c) 2015 learning. All rights reserved.
+//  Copyright © 2015 learning. All rights reserved.
 //
 
 import UIKit
 import SampleFramework
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let custom = CustomView()
-        print(custom.sampleString())
-        print(custom.sampleTestCount())
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -27,4 +25,6 @@ class ViewController: UIViewController {
     
     
 }
+
+
 
