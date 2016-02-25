53b6800fbb38a298c9599cd85619450920e95461
Hari Kunwar <hkunwar>
2016-02-25 14:58:14 -0800

    CollectionViewPaging: Horizontal paging in collection view.

---
+++ CollectionViewPaging/CollectionViewPaging/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  CollectionViewPaging
+//
+//  Created by Hari Kunwar on 2/25/16.
+//  Copyright © 2016 Learning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("collectionCell", forIndexPath: indexPath)
+        cell.backgroundColor = UIColor.redColor()
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+
+}
+
