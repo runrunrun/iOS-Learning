007145ea6f2e2ffbff7fcbe6f4ebcfe88b780ded
Hari Kunwar <hkunwar>
2016-08-22 16:26:33 -0700

    Started cloud kit key value sample.

---
+++ CloudKitSample/CloudKitSample/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CloudKitSample
+//
+//  Created by Hari Kunwar on 8/22/16.
+//  Copyright © 2016 Learning. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
