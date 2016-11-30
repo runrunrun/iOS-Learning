1109dda1c5a0e4de4896ae66bb1fae2fe59a03d7
Hari Kunwar <hkunwar>
2016-11-30 05:52:31 -0800

    Added Focus guides to move focus between grid buttons.

--- FocusGuide/FocusGuide/ViewController.swift
+++ FocusGuide/FocusGuide/ViewController.swift
@@ -13,6 +13,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Left edge focus guide
+        let leftFocusGuide = UIFocusGuide()
+        self.view.addLayoutGuide(leftFocusGuide)
+        var tag = 3
+        if let button = self.view.viewWithTag(tag) {
+            leftFocusGuide.widthAnchor.constraint(equalTo: button.widthAnchor).isActive = true
+            leftFocusGuide.heightAnchor.constraint(equalTo: button.heightAnchor).isActive = true
+            leftFocusGuide.leadingAnchor.constraint(equalTo: button.trailingAnchor, constant: 60).isActive = true
+            leftFocusGuide.centerYAnchor.constraint(equalTo: button.centerYAnchor).isActive = true
+            if let nextButton = self.view.viewWithTag(tag + 1) {
+                leftFocusGuide.preferredFocusEnvironments = [nextButton]
+            }
+        }
+        
+        // Right edge focus guide
+        let rightFocusGuide = UIFocusGuide()
+        self.view.addLayoutGuide(rightFocusGuide)
+        tag = 4
+        if let button = self.view.viewWithTag(tag) {
+            rightFocusGuide.widthAnchor.constraint(equalTo: button.widthAnchor).isActive = true
+            rightFocusGuide.heightAnchor.constraint(equalTo: button.heightAnchor).isActive = true
+            rightFocusGuide.trailingAnchor.constraint(equalTo: button.leadingAnchor, constant: -60).isActive = true
+            rightFocusGuide.centerYAnchor.constraint(equalTo: button.centerYAnchor).isActive = true
+            if let nextButton = self.view.viewWithTag(tag - 1) {
+                rightFocusGuide.preferredFocusEnvironments = [nextButton]
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +54,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func shouldUpdateFocus(in context: UIFocusUpdateContext) -> Bool {
+        var updateFocus = true
+        if context.focusHeading == .down || context.focusHeading == .up {
+            updateFocus = false
+        }
+        return updateFocus
+    }
+    
 }
 
