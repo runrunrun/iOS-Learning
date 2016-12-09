bc690aa037c2c28e8c3da497bceb2d21c283a631
Hari Kunwar <hkunwar>
2016-12-09 08:42:12 -0800

    OperatorOverloading: Added generic and custom overloading samples.

--- OperatorOverloading.playground/Contents.swift
+++ OperatorOverloading.playground/Contents.swift
@@ -3,12 +3,68 @@
 import UIKit
 
 
-func *(text: String, multiplier: Int) -> String {
-    var newText = text
-    for _ in 2...multiplier {
-        newText += text
-    }
-    return newText
+//func *(text: String, multiplier: Int) -> String {
+//    var newText = text
+//    for _ in 2...multiplier {
+//        newText += text
+//    }
+//    return newText
+//}
+//
+//"hello"*3
+
+
+// Generic Operators
+protocol Type {
+    static func +=( lhs: inout Self, rhs:Self)
+    static func +(lhs: Self, rhs: Self) -> Self
 }
 
-"hello"*3
+extension String: Type {}
+extension Int: Type {}
+extension Double: Type {}
+extension Float: Type {}
+
+
+func *<T: Type>(lhs: T, rhs: Int) -> T {
+    var result = lhs
+    for _ in 2...rhs {
+        result = result + lhs
+    }
+    return result
+}
+
+"Hello"*3
+3.0*4
+
+
+
+// Custom Operators
+
+infix operator ** {associativity left precedence 150}
+
+func **<T: Type>(lhs: T, rhs: Int) -> T {
+    var result = lhs
+    for _ in 2...rhs {
+        result = result + lhs
+    }
+    return result
+}
+
+"abc"**4
+
+
+// Compound Operators
+
+infix operator **= {associativity left precedence 150}
+
+func **=<T: Type>(lhs: inout T, rhs: Int) {
+    lhs = lhs ** rhs
+}
+var o = "abc"
+o **= 5
+
+var q = 2
+q **= 5
+
+
