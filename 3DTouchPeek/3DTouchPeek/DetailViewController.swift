988c57c3a3d7d0cce588e00a0e5b32342e2671db
Hari Kunwar <hkunwar>
2017-01-02 07:14:20 -0800

    Updated 3DTouchPeek to use Swift3. Added documentation.

--- 3DTouchPeek/3DTouchPeek/DetailViewController.swift
+++ 3DTouchPeek/3DTouchPeek/DetailViewController.swift
@@ -9,9 +9,7 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
     @IBOutlet weak var titleLabel: UILabel!
-    
     var detailText: String? {
         didSet {
             self.configureView()
@@ -19,7 +17,6 @@ class DetailViewController: UIViewController {
     }
     
     func configureView() {
-        // Update the user interface for the detail item.
         if let detail = self.detailText {
             if let label = self.titleLabel {
                 label.text = detail
@@ -30,23 +27,6 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureView()
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    
-
 }
--- 3DTouchPeek/3DTouchPeek/MasterViewController.swift
+++ 3DTouchPeek/3DTouchPeek/MasterViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+// Peek and Pop effect is setup in storyboard. 
+
 class MasterViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
@@ -15,43 +17,35 @@ class MasterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     // MARK: - UITableViewDataSource
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.names.count
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+    func tableView(_ tableView: UITableView, cellForRowAtIndexPath indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = self.names[indexPath.row]
         return cell
     }
     
-    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
         
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
                 controller.detailText = self.names[indexPath.row]
             }
         }
-        else if let cell = sender as? UITableViewCell where segue.identifier == "showDetailPeek" {
-            let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-            if let indexPath = self.tableView.indexPathForCell(cell) {
+        else if let cell = sender as? UITableViewCell, segue.identifier == "showDetailPeek" {
+            let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+            if let indexPath = self.tableView.indexPath(for: cell) {
                 controller.detailText = self.names[indexPath.row]
             }
         }
