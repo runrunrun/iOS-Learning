6e95cb8400c48abb30d047d5f9511cf6cfcb1311
Hari Kunwar <hkunwar>
2017-07-18 20:48:43 -0700

    KVO-iOS11: Used SwiftFormat to format the code.

--- KVO-iOS11/KVO-iOS11/Team.swift
+++ KVO-iOS11/KVO-iOS11/Team.swift
@@ -13,7 +13,7 @@ import Foundation
     let city: String
     let league: String
     
-    //The dynamic keyword is required for the observe closure to fire after a value change. Without it, observe will fail silently.
+    // The dynamic keyword is required for the observe closure to fire after a value change. Without it, observe will fail silently.
     // BUG: Cannot handle optional values eg. Int?
     dynamic var ranking: Int
     
--- KVO-iOS11/KVO-iOS11/ViewController.swift
+++ KVO-iOS11/KVO-iOS11/ViewController.swift
@@ -13,25 +13,24 @@ class ViewController: UIViewController {
     var observer: NSKeyValueObservation?
     @IBOutlet weak var rankLabel: UILabel!
     @IBOutlet weak var teamNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         team = Team(name: "Manchester United", city: "Manchester", league: "EPL", ranking: 5)
         teamNameLabel.text = team?.name
         
         // Initial rank
-        rankLabel.text = "Rank \(team.ranking)"
+        rankLabel.text = "Rank \(team!.ranking)"
         
-        
-        observer = team?.observe(\Team.ranking, changeHandler: { [weak self] (instance, change) in
+        observer = team?.observe(\Team.ranking, changeHandler: { [weak self] instance, _ in
             self?.rankLabel.text = "Rank \(instance.ranking.description)"
         })
     }
-    
 }
 
 // MARK: IBActions
 extension ViewController {
+    
     @IBAction func incrementButtonPressed(_ sender: Any) {
         if let rank = team?.ranking {
             team?.ranking = rank + 1
